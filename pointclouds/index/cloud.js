47c1ecb604f785a337d11f91fe9fab3e17950ea1
unknown <pe_gasa@hotmail.com>
2021-04-01 18:13:16 +0200

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,83 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 26609189,
+    "boundingBox": {
+        "lx": 581599.906,
+        "ly": 2643888.99,
+        "lz": 1784.497,
+        "ux": 582177.191,
+        "uy": 2644466.2750000006,
+        "uz": 2361.782000000033
+    },
+    "tightBoundingBox": {
+        "lx": 581599.906,
+        "ly": 2643888.99,
+        "lz": 1784.497,
+        "ux": 582177.191,
+        "uy": 2644461.769,
+        "uz": 1845.919
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 4.999434947967529,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
